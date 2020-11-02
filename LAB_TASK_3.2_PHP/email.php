aa51d0e206be89246b87a9e2833f344322280cf3
unknown <raselahamed613@gmail.com>
2020-11-03 00:23:43 +0600

    Lab_task3.2

---
+++ LAB_TASK_3.2_PHP/email.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Email</legend>
+        <form method="POST" action="">
+            <input type="text" name="email" id=""> 
+            <br>
+            <hr>
+            <input type="submit" name="submit" id="" value="Submit">
+        </form>
+    </fieldset>
+</body>
+</html>
+
+
+<?php
+
+
+if(isset($_POST['submit']))
+{
+    $email = $_POST["email"];
+    $atSign = strpos($email, "@");
+    $lastDot = strripos($email, ".");
+
+
+    if(!empty($_POST["email"]))
+    {
+        if($atSign > 0 && $email[$atSign+1] != "." && $lastDot > $atSign+1 && !strpos($email, " ") && !strpos($email, "..") && strlen($email) > ($lastDot+1))
+        {
+            echo "Your Email is $email";
+        }
+        else
+        {
+            echo "Invalid Email!";
+        }
+
+    }
+    else
+    {
+        echo "Please enter your Email";
+    }
+    
+
+    
+}
+
+
+?>
