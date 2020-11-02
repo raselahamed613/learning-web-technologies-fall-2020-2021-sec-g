6a6dff0d36d49bb74984ef1f243c24f8057afcb4
unknown <raselahamed613@gmail.com>
2020-11-03 00:00:44 +0600

    Lab task3.2

---
+++ LAB_TASK_3.2_PHP/blood.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <fieldset>
+        <legend>
+            Blood
+        </legend>
+        <form method="POST" >
+            <select name="blood_group" >
+                <option value="" ></option>
+				<option value="o+" >o+</option>
+				<option value="o-">o-</option>
+                <option value="B+">B+</option>
+                <option value="AB-">AB-</option>
+                <option value="A+">A+</option>
+                
+            </select> <br>
+            <hr>
+            <input type="submit" name="submit" id="" value="submit">
+        </form>
+    </fieldset>
+</body>
+</html>
+
+
+
+<?php
+
+
+    if(isset($_POST['submit']))
+    {
+        $bd = $_POST['blood_group'];
+
+        if(!empty($bd))
+        {
+            echo $bd;
+        }
+        else
+        {
+            echo 'Enter blood Group';
+        }
+        
+    }
+
+
+
+?>
