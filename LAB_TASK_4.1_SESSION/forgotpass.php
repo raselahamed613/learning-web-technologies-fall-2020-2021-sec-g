24ffc281a768023e38499fd0656c8ca1976eaf72
unknown <raselahamed613@gmail.com>
2020-11-04 23:44:51 +0600

    Lab_Task_4.1

---
+++ LAB_TASK_4.1_SESSION/forgotpass.php
@@ -0,0 +1,43 @@
+<?php
+include_once('header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Forgot Password</title>
+</head>
+<body >
+    <form action="">
+        <hr>
+        <br>
+        <fieldset>
+            <legend><b>FORGOT PASSWORD</legend>
+        <table align="center" >
+            <tr>
+                <td>Enter Email: </td>
+                <td><input type="text" name="fgemail"></td>
+            </tr>
+            <tr>
+                <td colspan="2">
+                    <hr>
+                    <input type="submit" name='submit'> 
+                </td>
+            </tr>
+        </table>
+        </fieldset>
+        <br>
+        
+    </form>
+    
+</body>
+</html>
+
+
+<?php
+
+    include_once('footer.html');
+    
+?>
