55960f235f0886d1c96feec7d029ef9eb62d3f9d
unknown <raselahamed613@gmail.com>
2020-11-05 00:21:53 +0600

    Lab_Task_4.1

---
+++ LAB_TASK_4.1_SESSION/view_profile.php
@@ -0,0 +1,51 @@
+<?php
+include_once('header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>profile</title>
+</head>
+<body>
+    <hr>
+    <br><br>
+    <table align="center" width='80%' border="">
+        <tr>
+            <td>
+                <h3>Account</h3>
+                <hr>
+                <ul>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">View Profile</a></li>
+                    <li><a href="">Edit Profile</a></li>
+                    <li><a href="">Change Profile Picture</a></li>
+                    <li><a href="">Change Password</a></li>
+                    <li><a href="">Log-Out</a></li>
+                </ul>                
+            </td>
+            <td>
+                <fieldset>
+                <legend>PROFILE</legend>
+                <p>Name : Bob</p>
+                <hr>
+                <p>Email : bob@aiub.edu</p>
+                <hr>
+                <p>Gender : Male</p>
+                <hr>
+                <p>Date of Birth:19/09/1998</p>
+                </fieldset>
+            </td>
+        </tr>
+        
+    </table>
+    <hr>
+</body>
+</html>
+
+<?php
+
+    include_once('footer.html');
+?>
