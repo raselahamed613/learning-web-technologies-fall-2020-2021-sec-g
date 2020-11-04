293cf78895bda732aae5f8f5c3bba217e739e454
unknown <raselahamed613@gmail.com>
2020-11-05 00:02:22 +0600

    Lab_Task_4.1

---
+++ LAB_TASK_4.1_SESSION/profileview.php
@@ -0,0 +1,42 @@
+<?php
+include_once('header.html');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Logged_Dashboard</title>
+</head>
+<body>
+    <hr>
+    <br><br>
+    <table align="center" width='80%' border="1">
+        <tr>
+            <td>
+                <h3>Account</h3>
+                <hr>
+                <ul>
+                    <li><a href="">Dashboard</a></li>
+                    <li><a href="">View Profile</a></li>
+                    <li><a href="">Edit Profile</a></li>
+                    <li><a href="">Change Profile Picture</a></li>
+                    <li><a href="">Change Password</a></li>
+                    <li><a href="">Log-Out</a></li>
+                </ul>                
+            </td>
+            <td>
+                <h2>Welcome Bob</h2>
+            </td>
+        </tr>
+        
+    </table>
+    <hr>
+</body>
+</html>
+
+<?php
+
+    include_once('footer.html');
+?>
