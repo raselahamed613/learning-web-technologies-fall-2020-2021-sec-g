8d4fa36907439e280c2a09f2d260c595c1a19267
unknown <raselahamed613@gmail.com>
2020-11-25 01:01:15 +0600

    Database_work

--- FINAL_LAB_TASK_1.1_DB/php/image_check.php
+++ FINAL_LAB_TASK_1.1_DB/php/image_check.php
@@ -1,13 +1,13 @@
 <?php
 	session_start();
 
-	if(isset($_FILES['mypic'])){
+	if(isset($_FILES['myImage'])){
 
-		$mydir 	= "../assets/".$_FILES['mypic']['name'];
-		$src	= $_FILES['mypic']['tmp_name'];
+		$mydir 	= "../image/".$_FILES['myImage']['name'];
+		$src	= $_FILES['myImage']['tmp_name'];
 
 		if(move_uploaded_file($src, $mydir)){
-			$_SESSION['img']= $_FILES['mypic']['name'];
+			$_SESSION['myImage']= $_FILES['myImage']['name'];
 			header('location: ../view/profile.php');
 		}else{
 			echo "ERROR!!";
